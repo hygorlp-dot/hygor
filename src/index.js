1800f1eb7e96e6502da1475675ab6f3bdf84a763
hygorlp-dot <hygorlp@gmail.com>
2026-07-14 09:49:49 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import AuthGate from './AuthGate';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import ErrorBoundary from "./ErrorBoundary";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <AuthGate>
+    <ErrorBoundary>
       <App />
-    </AuthGate>
+    </ErrorBoundary>
   </React.StrictMode>
 );
